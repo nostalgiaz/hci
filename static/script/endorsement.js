f466d7e3a2ccce33898db38611b4c440e252c73a
matteolever <lever.matteo@gmail.com>
2013-12-10 16:28:38 +0100

    endorsement on index.html

--- static/script/endorsement.js
+++ static/script/endorsement.js
@@ -1,4 +1,14 @@
+var open = false;
+
 $('#open').click(function() {
-		$('#menu').removeClass( 'menu-close' );
-		$('#menu').addClass( 'menu-open' );
+		if(open == false){
+			$('#menu').removeClass( 'menu-close' );
+			$('#menu').addClass( 'menu-open' );
+			open = true;
+		}
+		else if(open == true){
+			$('#menu').removeClass( 'menu-open' );
+			$('#menu').addClass( 'menu-close' );	
+			open = false;
+		}
 });
