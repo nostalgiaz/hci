00669ded7d6bb04eb72bd3458d84653fcd4633f4
Mattia Larentis <mattia@larentis.eu>
2013-12-17 10:48:07 +0100

    animated stars and charts

--- static/script/endorsement.js
+++ static/script/endorsement.js
@@ -1,21 +1,47 @@
-var open = false;
+(function () {
+  "use strict";
 
-$('#open').click(function() {
-		if(open == false){
-			$('#menu').removeClass( 'menu-close' );
-			$('#menu').addClass( 'menu-open' );
-			open = true;
-		}
-		else if(open == true){
-			$('#menu').removeClass( 'menu-open' );
-			$('#menu').addClass( 'menu-close' );	
-			open = false;
-		}
-});
+  var $skills = $('#skills')
+    , $menu = $('#menu')
+    , $open = $('#open');
 
-(function(){
-	$('.histogram-bar').each(function(){
-		var $this = $(this);
-		$this.width($this.data('val') + '%');
-	});
+  $open.click(function () {
+    $menu.toggleClass('menu-close menu-open');
+  });
+
+  $('.histogram-bar').each(function () {
+    var $this = $(this);
+    $this.width($this.data('val') + '%');
+  });
+
+  $skills.find('.fa-star').on('click', function () {
+    var $this = $(this)
+      , value = $this.data('name')
+      , el = '<h5 data-time="now" data-name="' + value + '"><strong>Camillo Ferrari</strong> ha confermato <strong>' + value + '</strong><em>23 dicembre 2013</em></h5>'
+      , $endorsement = $open.find('span')
+      , $endorsementStar = $open.find('.fa-star')
+      , $menuEl = $menu.find('.row')
+      , $oldEl = $menuEl.find('[data-time="now"][data-name="' + value + '"]')
+      , $bar = $this.siblings().find('.student-bar');
+
+    $this.toggleClass('star-colored');
+    $bar.toggleClass('bar-colored');
+    $endorsementStar.addClass('endorsement-colored');
+    setTimeout(function () {
+      $endorsementStar.removeClass('endorsement-colored');
+    }, 500);
+
+    if ($oldEl.length === 1) {
+      $oldEl.remove();
+      $endorsement.html(parseInt($endorsement.html(), 10) - 1);
+      $bar.data('val', parseInt($bar.data('val'), 10) - 1);
+      $bar.width($bar.data('val') + '%');
+      return;
+    }
+
+    $menuEl.prepend(el);
+    $endorsement.html(parseInt($endorsement.html(), 10) + 1);
+    $bar.data('val', parseInt($bar.data('val'), 10) + 1);
+    $bar.width($bar.data('val') + '%');
+  });
 })();
